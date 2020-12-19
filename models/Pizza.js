ce5eca59772147c17ea302291e32a285be8b1e37
G.B.Abadio <gilabadio@gmail.com>
2020-12-19 14:17:02 -0500

    added the user model

---
+++ models/Pizza.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+      required: "Please provide a name for your pizza",
+      trim: true,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    size: {
+      type: String,
+      required: true,
+      enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
+      default: "Large",
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+
+
+// creae the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+
+//sxport the pizza model
+module.exports = Pizza;
